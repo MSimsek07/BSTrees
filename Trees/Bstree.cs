790e39f85aeab67cd6a3f5032d0ecd52a7169552
Muhammet <muhammetsimsek306@gmail.com>
2022-06-04 20:44:25 +0300

    Proje dosyası ekle.

---
+++ Trees/Bstree.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class Bstree
+    {
+        public Node root;
+        public Bstree()
+        {
+            root = null;
+        }
+        public Node newNode(int data)
+        {
+            root = new Node(data);
+            Console.WriteLine(data+" ağaca eklendi ");
+            return root;
+        }
+        public Node insert(Node root, int data)
+        {
+            if (root != null)
+            {
+                if (data< root.data)
+                {
+                    root.left = insert(root.left, data);
+                }
+                else
+                {
+                    root.right = insert(root.right, data);
+                }
+            }
+            else
+            {
+                root=newNode(data);
+            }
+            return root;
+        }
+        public Boolean search(Node root,int wanted) // SINAVDA GELSE BUNU YAZ
+        {
+            bool found = false;
+            while ((root != null) && !found)
+            {
+                int rval = root.data;
+                if (wanted < rval)
+                    root = root.left;
+                else if (wanted > rval)
+                    root = root.right;
+                else
+                {
+                    found = true;
+                    break;
+                }
+                found = search(root, wanted);
+            }
+            return found;
+
+        }
+        public Node enKucukBul(Node root) // EN KUCUK BULMA RECURSIVE
+        {
+            if( root.left != null)
+            {
+                    return enKucukBul(root.left);
+            }
+            return root;
+        }
+        public Node enBuyukBul(Node root)  // EN BUYUK BULMA RECURSIVE
+        {
+            
+            if (root.right != null)
+            {
+                return enBuyukBul(root.right);
+            }
+
+            return root;
+        }
+        public Node search_Recursive(Node root, int key)
+        {
+          
+            if (root == null || root.data == key)
+                return root;
+            
+            if (root.data > key)
+                return search_Recursive(root.left, key); 
+
+            return search_Recursive(root.right, key);
+        }
+        public Node search(int data, Node root)
+        {
+
+           
+            if (root == null || root.data == data)
+            {
+                return root;
+            }
+
+          
+            else if (data < root.data)
+            {
+                return search(data, root.left);
+            }
+
+           
+            return search(data, root.right);
+        }
+
+    }
+}
---
+++ Trees/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bstree bt = new Bstree();
+            bt.root = bt.insert(bt.root,15);
+            bt.root = bt.insert(bt.root,20);
+            bt.root = bt.insert(bt.root,25);
+            bt.root = bt.insert(bt.root,22);
+            bt.root = bt.insert(bt.root,5);
+            bt.root = bt.insert(bt.root,1);
+            bt.root = bt.insert(bt.root,100);
+            int enkucuk = bt.enKucukBul(bt.root).data;
+            Console.WriteLine("EN KUCUK ELEMAN : "+ enkucuk);
+            int enbuyuk = bt.enBuyukBul(bt.root).data;
+            Console.WriteLine("EN BUYUK ELEMAN : " + enbuyuk);
+            //Console.Write("Aranacak düğümü yazınız :");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //if (bt.search(bt.root, x)== true)
+            //{
+            //    Console.WriteLine("Aranan düğüm bulundu");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Aranan düğüm bulunamadı");
+            //}
+            //Console.WriteLine("Kökün solundaki düğüm => "+bt.root.left.data );
+            //Console.WriteLine("Kökün sağındaki düğüm => "+bt.root.right.data );
+            //Console.WriteLine("Kökün sağının sağındaki düğüm => "+bt.root.right.right.data );
+            //Console.WriteLine("Kökün sağının sağının solundaki düğüm => "+bt.root.right.right.left.data );
+
+
+            Console.ReadLine();
+        }
+    }
+}
