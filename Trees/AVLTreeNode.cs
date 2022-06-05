0c143e49572600453d9165db7be8a242786a13ee
Muhammet <muhammetsimsek306@gmail.com>
2022-06-05 18:46:26 +0300

    AVL tree implementation

---
+++ Trees/AVLTreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class AVLTreeNode
+    {
+        public int data;
+        public AVLTreeNode right;
+        public AVLTreeNode left;
+        public int hight=0;
+        public AVLTreeNode(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            
+        }
+        
+    }
+}
---
+++ Trees/AVLtree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class AVLtree
+    {
+        AVLTreeNode root;
+
+        public int height(AVLTreeNode node)
+        {
+            if (node == null)
+            {
+                return -1;
+
+            }
+            else
+                return node.hight;
+
+        }
+        public int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+
+    }
+}
